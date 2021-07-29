b7b8c30a72cafe408b4e414c98f51dd8af78196c
YriiHvozdetskiy <hvozdetskiyyrii@gmail.com>
2021-07-29 10:23:22 +0300

    delete commit Modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,31 +1,28 @@
-import { Component } from 'react';
 import { createPortal } from 'react-dom';
+import { useEffect } from 'react';
 
 const modalRoot = document.querySelector('#modal-root');
 
-export class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
+export const  Modal =({closeModal,children})=> {
+  useEffect(()=>{
+    window.addEventListener('keydown',handleKeyDown);
+    return ()=>{
+      window.removeEventListener('keydown',handleKeyDown);
+    }
+  })
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
-
-  handleKeyDown = e => {
-    if (e.code === 'Escape') return this.props.closeModal();
+  const handleKeyDown = e => {
+    if (e.code === 'Escape') return closeModal();
   };
 
-  handleBackdropClick = e => {
-    if (e.target === e.currentTarget) return this.props.closeModal();
+  const handleBackdropClick = e => {
+    if (e.target === e.currentTarget) return closeModal();
   };
 
-  render() {
     return createPortal(
-      <div className="Overlay" onClick={this.handleBackdropClick}>
-        <div className="Modal">{this.props.children}</div>
+      <div className="Overlay" onClick={handleBackdropClick}>
+        <div className="Modal">{children}</div>
       </div>,
       modalRoot,
     );
-  }
 }
